d655f9bebb2a860afabe93a65f995bd876aca53b
Ankalimel <33578302+Ankalimel@users.noreply.github.com>
2022-12-10 01:03:19 +0500

    Now all the tasks can be correctly deleted

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-const tasks = document.getElementsByTagName('LI');
-let i;
+const tasks = document.querySelectorAll('.tasks li');
+const trash = document.querySelectorAll('.trashbin');
+const add = document.querySelectorAll('h5 img');
 
-for(i=0;i<tasks.length;i++){
+for(let i=0;i<tasks.length;i++){
 	tasks[i].addEventListener('click', function(t) {
-  if (t.target.tagName === 'LI') {
+  if (t.target.tagName.toLowerCase() === 'li') {
     t.target.classList.toggle('checked');
   }
 });
 }
 
+for (let i = 0; i<trash.length; i++) {
+  trash[i].onclick = function() {
+    let a = this.parentElement;
+    a.style.display = "none";
+  }
+}
+
+// for (let i = 0; i<add.length; i++) {
+//   add[i].onclick = function() {
+//     let b = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
