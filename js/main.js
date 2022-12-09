2a86260043040081bc642a9222ece0fec182ac4a
Ankalimel <33578302+Ankalimel@users.noreply.github.com>
2022-12-09 13:54:29 +0500

    Added toggle for checkboxes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-const task = document.getElementsByTagName('LI');
+const tasks = document.getElementsByTagName('LI');
+const dots = document.getElementsByTagName('SPAN');
 let i;
 
-for(i=0;i<task.length;i++){
-	task[i].addEventListener('click', function(t) {
+for(i=0;i<tasks.length;i++){
+	tasks[i].addEventListener('click', function(t) {
   if (t.target.tagName === 'LI') {
     t.target.classList.toggle('checked');
+    t.target.classList.toggle('completed');
   }
 });
 }
